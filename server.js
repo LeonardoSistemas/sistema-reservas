7374654506e2832a5dc99bd149eeb20ebc54c482
Leonardo Treviso <leonardosistemas@outlook.com>
2024-09-25 17:08:37 -0300

    adicionando dependencia mongoose no arquivo server

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
 // Middleware para permitir o uso de JSON nas requisições
 app.use(express.json());
+
+// Conectando ao MongoDB
+mongoose.connect('mongodb://localhost:27017/reservas_salas', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => {
+    console.log('Conectado ao MongoDB');
+})
+.catch((error) => {
+    console.log('Erro ao conectar ao MongoDB:', error);
+});
 
 // Importando as rotas
 const reservaRoutes = require('./src/routes/reservaRoutes');
